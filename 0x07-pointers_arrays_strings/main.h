aa6a2e97232422e01b849e15b877142788b6e2fe
YaYa-SKY <yahia.seqqai@gmail.com>
2023-04-03 16:39:32 -0700

    added putchar funtion to the main header

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -9,4 +9,6 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 void print_chessboard(char (*a)[8]);
 void set_string(char **s, char *to);
+int _putchar(char c);
+
 #endif /* MAIN_H */
