8abf8c1cbc9258f6ab4a2516bd659b4701480fef
YaYa-SKY <yahia.seqqai@gmail.com>
2023-05-05 10:40:04 -0700

    updated

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,30 @@
+/**
+ * print_binary - prints an unsigned int in binary
+ *
+ * @n: unsigned int to print in binary
+ *
+ * Return: void
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int printbit = 1ul << 63;
+	char c = '0';
+
+	/* find the highest bit that is set */
+	while (!(printbit & n) && printbit != 0)
+		printbit = printbit >> 1;
+
+	if (printbit == 0) /* handle the case where n is 0 */
+		write(1, &c, 1);
+
+	/* print the binary representation of n */
+	while (printbit)
+	{
+		if (printbit & n)
+			c = '1';
+		else
+			c = '0';
+		write(1, &c, 1);
+		printbit = printbit >> 1;
+	}
+}
