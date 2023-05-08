5f69f003566c8f70ce0287a655f6b334bf0e43ee
YaYa-SKY <yahia.seqqai@gmail.com>
2023-05-08 16:28:50 -0700

    updated

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - Function that creates a file
+ * @filename: type char filename pointer of FD
+ * @text_content: type char pointer of char
+ *
+ * Return: -1 if NULL in FN, 1 on success
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+    int file, final_out, x;
+
+    file = open(filename, O_RDWR | O_APPEND);
+    if (filename == NULL)
+        return (-1);
+    if (file == -1)
+        return (-1);
+    if (text_content == NULL)
+        return (1);
+
+    x = 0;
+    while (*(text_content + x) != '\0')
+        x++;
+    final_out = write(file, text_content, x);
+    if (final_out == -1)
+        return (-1);
+
+    return (1);
+}
